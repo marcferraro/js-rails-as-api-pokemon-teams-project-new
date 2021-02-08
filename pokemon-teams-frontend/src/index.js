c8b8527df83685bba6c261d3fbf9921c0b6aa332
Marc <marc.ferraro1@gmail.com>
2021-02-08 11:12:13 -0600

    Fetch and Part of Render trainers

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,24 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+function main(){
+    console.log("I wanna be the very best")
+    fetchTrainers()
+}
+
+function fetchTrainers(){
+    fetch(TRAINERS_URL)
+    .then(resp => resp.json())
+    .then(trainers => trainers.forEach(trainer => renderTrainer(trainer)))
+}
+
+function renderTrainer(trainer){
+    const container = document.getElementById('trainer-container')
+    
+    const divNode = document.createElement('div')
+    divNode.className="card"
+    divNode.dataset.id=trainer.id
+}
+
+main()
